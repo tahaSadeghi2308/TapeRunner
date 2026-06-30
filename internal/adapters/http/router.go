7b87c8bc5b3526ed8743763034b0b6f27787c630
Maryam-Nokohan <maryam.nokohan@gmail.com>
2026-06-30 20:21:14 +0330

    feat : route the handlers

--- internal/adapters/http/router.go
+++ internal/adapters/http/router.go
@@ -1,1 +1,15 @@
 package http
+
+import "net/http"
+
+func SetupRouter(handler *Handler) *http.ServeMux {
+	mux := http.NewServeMux()
+
+	fs := http.FileServer(http.Dir("web/static"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	mux.HandleFunc("/", handler.ServeUI)
+	mux.HandleFunc("/api/run", handler.HandleRun)
+
+	return mux
+}
